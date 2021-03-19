09f2b8455468d035ca901c6a4ee91054b25a7e90
Cengiz <cengiz1ergun@gmail.com>
2021-03-20 01:26:10 +0300

    temel closure

--- code.py
+++ code.py
@@ -1,8 +1,14 @@
-from functools import reduce
+def adder(n):
+    def inner(x):
+        return x + n
+    return inner
 
 
-def factorial(number):
-    return reduce(lambda x, y: x * y, range(1, number + 1))
+adder_1 = adder(10)
+adder_2 = adder(10)
+adder_3 = adder(10)
+# bir kısayaol?
 
-
-print(factorial(5))
+print(adder_1(1))
+print(adder_2(2))
+print(adder_3(3))

f572d2ace944dcf0fbfe95331a74d056c84323ba
Musa Büyükkaba <65968506+Trunday@users.noreply.github.com>
2021-03-20 11:20:37 +0300

    Update code.py
    Ekleme yapıldı

--- code.py
+++ code.py
@@ -6,3 +6,6 @@ for n in range(1, 4):
 print(adders[0](10))  # sonuç 11.
 # Bu durum aslında fonksiyon yaratılmasıyla ilişkili. Fonksiyon parametresine atanan varsayılan değer aynen tutulur ve fonksiyon 
 # çağırılırken de bu değer kullanılır. 
+
+ms = [ 2, 6, 6, 7, 8]
+print(ms[3])
